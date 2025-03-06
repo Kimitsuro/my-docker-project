9017a5368dcb244e0ed715bfb08eb8821c9058a1
Kimitsu <dim.bobenko201393@yandex.ru>
2025-03-06 13:59:25 +0300

    On branch lab2 Your branch is up to date with 'origin/lab2'.
    Changes to be committed:
    	modified:   src/data.csv
    	modified:   src/form.php

--- src/form.php
+++ src/form.php
@@ -6,13 +6,37 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $master = $_POST['master'] ?? '';
     $datetime = $_POST['datetime'] ?? '';
 
-    if (!empty($name) && !empty($phone) && !empty($service) && !empty($master) && !empty($datetime)) {
+    $errors = [];
+
+    if (empty($name)) {
+        $errors[] = "Имя обязательно для заполнения.";
+    }
+
+    if (empty($phone)) {
+        $errors[] = "Телефон обязателен для заполнения.";
+    } elseif (!preg_match('/^\d{9}(\d{2})?$/', $phone)) {
+        $errors[] = "Номер телефона должен содержать 9 или 11 цифр.";
+    }
+
+    if (empty($datetime)) {
+        $errors[] = "Дата и время обязательны для заполнения.";
+    } else {
+        $selectedDateTime = new DateTime($datetime);
+        $currentDateTime = new DateTime();
+        if ($selectedDateTime < $currentDateTime) {
+            $errors[] = "Нельзя записаться на прошедшую дату.";
+        }
+    }
+
+    if (empty($errors)) {
         $file = fopen("data.csv", "a");
         fputcsv($file, [$name, $phone, $service, $master, $datetime], ";");
         fclose($file);
         echo "Запись успешно сохранена!";
     } else {
-        echo "Ошибка: заполните все поля!";
+        foreach ($errors as $error) {
+            echo "<p>Ошибка: $error</p>";
+        }
     }
 }
 ?>
